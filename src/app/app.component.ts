e05937fb8a20f41092dfade3de0af86b823700cf
facusteckler86 <facundomsteckler@gmail.com>
2024-06-18 01:18:04 -0300

    arreglo del menu y del aside

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeadComponent } from "./comoponentes/head/head.component";
+import { MenuComponent } from './comoponentes/menu/menu.component';
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeadComponent,MenuComponent]
 })
+
+
+
+
 export class AppComponent {
   title = 'Angular';
 }
+
+
+
+
+
+
---
+++ src/app/comoponentes/head/head.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-head',
+  standalone: true,
+  imports: [],
+  templateUrl: './head.component.html',
+  styleUrl: './head.component.css'
+})
+export class HeadComponent {
+
+}
+
+
+
